be5648dc90c22036484909b544eb60ffeefad124
yadhukrishnaofficial <yadhukrish605@gmail.com>
2025-07-02 11:39:34 +0530

    fixed input field error in address form

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 
 export default function FormInput({
-  type,
+  type = "text",
   placeholder,
   value,
-  onchange,
+  onChange,
   title,
 }) {
   return (
     <div className="form-row">
-      <div className="flex  flex-col gap-1">
+      <div className="flex flex-col gap-1">
         {title && <label className="text-sm text-left">{title}</label>}
         <input
           type={type}
           className="w-full py-1 px-3 rounded-lg bg-[#E1E1E1] border border-gray-300 text-gray-900 h-11"
           placeholder={placeholder}
           value={value}
-          onChange={onchange}
+          onChange={onChange} 
           required
         />
       </div>
